8d7d1d6ff0ee4c7a0689b2b05e49d1db9b301a30
Eduardo Tashiro <eduardogaia854@hotmail.com>
2025-12-07 23:13:58 -0300

    fead: initialize logic

---
+++ src/slack/notifier/slackNotifier.ts
@@ -0,0 +1,7 @@
+// if (s.communicate) {
+//   chamaslack(s.company, `Oficialmente fora do ar: ${s.communicate}`);
+// } else if (latestReport > maxBaseline) {
+//   chamaslack(s.company, `Possível instabilidade: ${latestReport} notificações vs baseline ${maxBaseline}`);
+// } else {
+//   console.log(`${s.company}  OK`);
+// }//
