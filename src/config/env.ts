84077a3c5e83e18d320dcba3c24cb55d236f8c78
Eduardo Tashiro <eduardogaia854@hotmail.com>
2025-12-09 15:01:18 -0300

    feat: add slack channel

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,6 +5,7 @@ export const config = {
     slack:{
         botToken: process.env.SLACK_BOT_TOKEN!,
         signingSecret: process.env.SLACK_SIGNING_SECRET!,
+        channel: process.env.CHANNEL_ID!
     },
     slackId:{
         Eduardo:process.env.EDUARDO!
