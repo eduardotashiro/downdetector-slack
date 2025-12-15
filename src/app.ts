e5a00f2b56b3cccdf9a678eb097edabc37097bdb
Eduardo Tashiro <eduardogaia854@hotmail.com>
2025-12-15 13:19:31 -0300

    fix: i forget .js in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import { App, LogLevel } from "@slack/bolt";
-import { config } from "./config/env";
+import { config } from "./config/env.js";
 // import cron from "node-cron";
-import { CheckAll } from "./slack/notifier/batchNotifier";
+import { CheckAll } from "./slack/notifier/batchNotifier.js";
 
 export const app = new App({
   signingSecret: config.slack.signingSecret,
