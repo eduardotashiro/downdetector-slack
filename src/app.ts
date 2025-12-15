0908b5446ed781e8d23b60a5f619eaf8162fd833
Eduardo Tashiro <eduardogaia854@hotmail.com>
2025-12-15 18:31:27 -0300

    feat, monitor 30/30min

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import { App, LogLevel } from "@slack/bolt";
 import { config } from "./config/env.js";
-// import cron from "node-cron";
+import cron from "node-cron";
 import { CheckAll } from "./slack/notifier/batchNotifier.js";
 
 export const app = new App({
@@ -9,9 +9,8 @@ export const app = new App({
   token: config.slack.botToken,
 });
 
-// cron.schedule("*/30 7-22 * * *", async () => {
-//   await CheckAll();
-// console.log("Monitoramento finalization!");
-// });
+cron.schedule("*/30 7-22 * * *", async () => {
+  await CheckAll();
+console.log("Monitoramento finalization!");
+});
 
- CheckAll();
