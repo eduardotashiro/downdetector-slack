1775ebf8213a0c5b896850f8826f7bdc28c473ed
Eduardo Tashiro <eduardogaia854@hotmail.com>
2025-12-15 18:55:12 -0300

    agora só manutenção, configurado para canal novo

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import { App, LogLevel } from "@slack/bolt";
 import { config } from "./config/env.js";
-// import cron from "node-cron";
+import cron from "node-cron";
 import { CheckAll } from "./slack/notifier/batchNotifier.js";
 
 export const app = new App({
@@ -9,10 +9,10 @@ export const app = new App({
   token: config.slack.botToken,
 });
 
-// cron.schedule("*/30 7-22 * * *", async () => {
-//   await CheckAll();
-// console.log("Monitoramento finalization!");
-// });
+cron.schedule("*/30 7-22 * * *", async () => {
+  await CheckAll();
+console.log("Monitoramento finalization!");
+});
 
-CheckAll()
+
 
