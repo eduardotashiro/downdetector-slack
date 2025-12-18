293955887d9490afc578f396b918043d1ef42f94
Eduardo Tashiro <eduardogaia854@hotmail.com>
2025-12-18 10:37:04 -0300

    cron 24/7

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ export const app = new App({
   token: config.slack.botToken,
 });
 
-cron.schedule("*/30 7-22 * * *", async () => {
+cron.schedule("*/30 * * * *", async () => {
   await CheckAll();
 console.log("Monitoramento finalization!");
 });
