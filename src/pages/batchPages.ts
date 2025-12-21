bac6ebb7efad4561c6ff691b3f8ca2f9dfefe574
Eduardo Tashiro <eduardogaia854@hotmail.com>
2025-12-21 05:04:11 -0300

    feat: wait only window.DD....status :p

--- src/pages/batchPages.ts
+++ src/pages/batchPages.ts
@@ -5,27 +5,27 @@ chromium.use(stealth());
 
 //Bradesco
 const SERVICES = [
-  { name: "Pix", url: "https://downdetector.com.br/fora-do-ar/pix/" },
-  { name: "Itaú", url: "https://downdetector.com.br/fora-do-ar/banco-itau/" },
-  { name: "Bradesco", url: "https://downdetector.com.br/fora-do-ar/bradesco/" },
-  {
-    name: "Santander",
-    url: "https://downdetector.com.br/fora-do-ar/santander/",
-  },
-  { name: "Nubank", url: "https://downdetector.com.br/fora-do-ar/nubank/" },
-  {
-    name: "Bancodobrasil",
-    url: "https://downdetector.com.br/fora-do-ar/banco-do-brasil/",
-  },
-  // { name: 'Cloudflare', url: 'https://downdetector.com.br/fora-do-ar/cloudflare/' }
-  // { name: 'Azure', url: 'https://downdetector.com.br/fora-do-ar/windows-azure/' }
-  //{ name: 'Clearsale', url: 'https://statusgator.com/services/clearsale' }
-  //{ name: 'Rede', url: 'https://downdetector.com.br/fora-do-ar/rede/' }
-  //{ name: 'Getnet', url: 'https://downdetector.com.br/fora-do-ar/getnet/' }
-  //{ name: 'Cielo', url: 'https://downdetector.com.br/fora-do-ar/cielo/' }
-  //{ name: 'Pagbank', url: 'https://downdetector.com.br/fora-do-ar/pagbank/' }
-  //{ name: 'Mercadopago', url: 'https://downdetector.com.br/fora-do-ar/mercadopago/' }
-  //{ name: 'Safrapay', url: 'https://downdetector.com.br/fora-do-ar/safrapay/' }
+    { name: "Pix", url: "https://downdetector.com.br/fora-do-ar/pix/" },
+    { name: "Itaú", url: "https://downdetector.com.br/fora-do-ar/banco-itau/" },
+    { name: "Bradesco", url: "https://downdetector.com.br/fora-do-ar/bradesco/" },
+    {
+        name: "Santander",
+        url: "https://downdetector.com.br/fora-do-ar/santander/",
+    },
+    { name: "Nubank", url: "https://downdetector.com.br/fora-do-ar/nubank/" },
+    {
+        name: "Bancodobrasil",
+        url: "https://downdetector.com.br/fora-do-ar/banco-do-brasil/",
+    },
+    // { name: 'Cloudflare', url: 'https://downdetector.com.br/fora-do-ar/cloudflare/' }
+    // { name: 'Azure', url: 'https://downdetector.com.br/fora-do-ar/windows-azure/' }
+    //{ name: 'Clearsale', url: 'https://statusgator.com/services/clearsale' }
+    //{ name: 'Rede', url: 'https://downdetector.com.br/fora-do-ar/rede/' }
+    //{ name: 'Getnet', url: 'https://downdetector.com.br/fora-do-ar/getnet/' }
+    //{ name: 'Cielo', url: 'https://downdetector.com.br/fora-do-ar/cielo/' }
+    //{ name: 'Pagbank', url: 'https://downdetector.com.br/fora-do-ar/pagbank/' }
+    //{ name: 'Mercadopago', url: 'https://downdetector.com.br/fora-do-ar/mercadopago/' }
+    //{ name: 'Safrapay', url: 'https://downdetector.com.br/fora-do-ar/safrapay/' }
 ];
 //  '--no-sandbox',
 //     '--disable-setuid-sandbox',
@@ -33,124 +33,127 @@ const SERVICES = [
 //     '--disable-gpu','--disable-dev-shm-usag
 
 async function tentarAcessarServico(page: any, service: any) {
-  await page.goto(service.url, {
-    waitUntil: "domcontentloaded",
-    timeout: 60000,
-  });
+    await page.goto(service.url, {
+        waitUntil: "domcontentloaded",
+        timeout: 40000,
+    });
 
-  await page.waitForTimeout(1000);
+    await page.waitForTimeout(500);
 
-  const titulo = await page.title();
-  if (titulo.includes("momento") || titulo.includes("Um momento")) {
-    console.log(`Cloudflare detectado...`);
-    await page.waitForTimeout(6000);
-  }
+    const titulo = await page.title();
+    if (titulo.includes("momento") || titulo.includes("Um momento")) {
+        console.log(`Cloudflare detectado...`);
+        await page.waitForTimeout(4000);
+    }
 
-  await page.waitForFunction(
-    () => {
-      return window.DD?.currentServiceProperties !== undefined;
-    },
-    { timeout: 15000, polling: 500 }
-  );
+    await page.waitForFunction(
+        () => {
+            return window.DD?.currentServiceProperties?.status !== undefined;
+        },
+        { timeout: 12000, polling: 500 }
+    );
 
-  await page.waitForTimeout(1500);
+    await page.waitForTimeout(1000);
 
-  return await page.evaluate(() => {
-    return window.DD?.currentServiceProperties;
-  });
+    return await page.evaluate(() => {
+        return window.DD?.currentServiceProperties;
+        
+    });
+
+    
 }
 
 export async function checkAllServices() {
-  const browser = await chromium.launch({
-    args: ["--no-sandbox", "--disable-blink-features=AutomationControlled"],
-    headless: true,
-    proxy: {
-      server: config.proxy.server,
-      username: config.proxy.username,
-      password: config.proxy.password,
-    },
-  });
-  const context = await browser.newContext({
-    ignoreHTTPSErrors: true,
-    locale: "pt-BR",
-    timezoneId: "America/Sao_Paulo",
-    userAgent:
-      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
-  });
+    const browser = await chromium.launch({
+        args: ["--no-sandbox", "--disable-blink-features=AutomationControlled"],
+        headless: true,
+        proxy: {
+            server: config.proxy.server,
+            username: config.proxy.username,
+            password: config.proxy.password,
+        },
+    });
+    const context = await browser.newContext({
+        ignoreHTTPSErrors: true,
+        locale: "pt-BR",
+        timezoneId: "America/Sao_Paulo",
+        userAgent:
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
+    });
 
-  const resultados = [];
+    const resultados = [];
 
-  try {
-    for (const service of SERVICES) {
-      console.log(`analisando o serviço: ${service.name}...`);
+    try {
+        for (const service of SERVICES) {
+            console.log(`analisando o serviço: ${service.name}...`);
 
-      let page;
+            let page;
 
-      try {
-        page = await context.newPage();
+            try {
+                page = await context.newPage();
 
-        await page.route("**/*", (route) => {
-          const url = route.request().url();
-          const type = route.request().resourceType();
+                await page.route("**/*", (route) => {
+                    const url = route.request().url();
+                    const type = route.request().resourceType();
 
-          if (
-            url.includes("google-analytics") ||
-            url.includes("googletagmanager") ||
-            url.includes("gtag") ||
-            url.includes("facebook.com/tr") ||
-            url.includes("facebook.net") ||
-            url.includes("doubleclick") ||
-            url.includes("hotjar") ||
-            url.includes("/ads/") ||
-            url.includes("analytics")
-          ) {
-            return route.abort();
-          }
+                    if (
+                        url.includes("google-analytics") ||
+                        url.includes("googletagmanager") ||
+                        url.includes("gtag") ||
+                        url.includes("facebook.com/tr") ||
+                        url.includes("facebook.net") ||
+                        url.includes("doubleclick") ||
+                        url.includes("hotjar") ||
+                        url.includes("/ads/") ||
+                        url.includes("analytics")
+                    ) {
+                        return route.abort();
+                    }
 
-          if (["document", "script", "xhr", "fetch"].includes(type)) {
-            route.continue();
-          } else {
-            route.abort();
-          }
-        });
+                    if (["document", "script", "xhr", "fetch"].includes(type)) {
+                        route.continue();
+                    } else {
+                        route.abort();
+                    }
+                });
 
-        let dados;
+                let dados;
 
-        try {
-          dados = await tentarAcessarServico(page, service);
-        } catch (err) {
-          console.log(`Timeout / Cloud / destroyer... tentando novamente...`);
-          await page.close();
+                try {
+                    dados = await tentarAcessarServico(page, service);
+                } catch (err) {
+                    console.log(`Timeout / Cloud / destroyer... tentando novamente...`);
+                    await page.close();
 
-          page = await context.newPage();
-          dados = await tentarAcessarServico(page, service);
+                    page = await context.newPage();
+                    dados = await tentarAcessarServico(page, service);
+                }
+
+                if (dados) {
+                    resultados.push({
+                        nome: service.name,
+                        url: service.url,
+                        dados,
+                    });
+
+                    console.log(`----> ${service.name}`);
+                    console.log(`----> Status: ${dados.status}`);
+                    console.log(`----> Max: ${dados.max}`);
+                }
+            } catch (error) {
+                console.log(`${service.name}: ${(error as Error).message}`);
+            } finally {
+                if (page) {
+                    await page.close();
+                }
+            }
         }
 
-        if (dados) {
-          resultados.push({
-            nome: service.name,
-            url: service.url,
-            dados,
-          });
-
-          console.log(`----> ${service.name}`);
-          console.log(`----> Status: ${dados.status}`);
-          console.log(`----> Max: ${dados.max}`);
-        }
-      } catch (error) {
-        console.log(`${service.name}: ${(error as Error).message}`);
-      } finally {
-        if (page) {
-          await page.close();
-        }
-      }
+        console.log(
+            `\nFinalization: ${resultados.length}/${SERVICES.length} serviços`
+        );
+        return resultados;
+    } finally {
+        await browser.close();
     }
-
-    console.log(
-      `\nFinalization: ${resultados.length}/${SERVICES.length} serviços`
-    );
-    return resultados;
-  } finally {
-    await browser.close();
-  }
 }
