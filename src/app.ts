0aacb3092224dc5d93dbbd1f2196894e91e62697
Eduardo Tashiro <eduardogaia854@hotmail.com>
2025-12-23 00:36:15 -0300

    feat: update cronjob-v1.0

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,8 @@ export const app = new App({
   logLevel: LogLevel.INFO,
   token: config.slack.botToken,
 });
-// a cada 15 para bater com o site, vamos ver o que acontece 
-cron.schedule("*/15 * * * *", async () => {
+//mudei de ideia das 6h as 23h rodando de 15 em 15 min, ta bom até demais 
+cron.schedule("*/15 6-23 * * *", async () => {
  await CheckAll();
 console.log("Monitoramento finalization! ",new Date().toLocaleString("pt-BR", { timeZone: "America/Sao_Paulo" }));
 },{
