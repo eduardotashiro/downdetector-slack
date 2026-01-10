ab4befd1d8143b194905daaf697ac763022f0fc8
Eduardo Tashiro <eduardogaia854@hotmail.com>
2026-01-10 03:18:23 -0300

    666

--- src/pages/batchPages.ts
+++ src/pages/batchPages.ts
@@ -18,28 +18,23 @@ const SERVICES = [
     { name: "Nubank", url: "https://downdetector.com.br/fora-do-ar/nubank/" },
     { name: "Banco do Brasil", url: "https://downdetector.com.br/fora-do-ar/banco-do-brasil/" },
     { name: "Mercado Pago", url: "https://downdetector.com.br/fora-do-ar/mercadopago/" },
-    { name: "Pic Pay", url: "https://downdetector.com.br/fora-do-ar/picpay/" },
+    { name: "Pic Pay", url: "https://downdetector.com.br/fora-do-ar/picpay/" }
 ];
 
-//disfarçando 
+
 const delay = (min = 4000, max = 9000) =>
     new Promise(res =>
         setTimeout(res, Math.floor(Math.random() * (max - min + 1)) + min)
     );
 
 
-
-//
 async function tentarAcessarServico(page: any, service: any) {
     await page.goto(service.url, {
         waitUntil: "domcontentloaded",
         timeout: 60000,
     });
 
-    await page.waitForFunction(
-        () => window.DD?.currentServiceProperties !== undefined,
-        { timeout: 60000, polling: 800 }
-    );
+    await page.waitForFunction(() => window.DD?.currentServiceProperties !== undefined, { timeout: 60000, polling: 800 });
 
     return await page.evaluate(() => window.DD?.currentServiceProperties);
 }
@@ -62,7 +57,7 @@ export async function checkAllServices() {
 
     try {
         for (const service of SERVICES) {
-            const page = await context.newPage();
+            let page = await context.newPage();
 
             try {
                 const dados = await tentarAcessarServico(page, service);
@@ -83,8 +78,13 @@ export async function checkAllServices() {
                 try {
                     console.log("deu ruim:", error)
                     console.log("tentando novamente...")
-                    const dados = await tentarAcessarServico(page, service);
+                    await page.close();
+                    console.log(`aba fechada...`)
+                    page = await context.newPage()
+                    console.log(`aba aberta...`)
 
+                    const dados = await tentarAcessarServico(page, service);
+                    console.log(`acessando os serviços no segundo erro.. `)
                     if (dados) {
                         resultados.push({
                             nome: service.name,
@@ -93,6 +93,8 @@ export async function checkAllServices() {
                         });
 
                         console.log(`💀 ${service.name}: ${dados.status}`);
+
+
                     }
                 } catch (e) {
                     console.log("falhou duas vezes bora tratar essa merda")
@@ -105,19 +107,19 @@ export async function checkAllServices() {
                         ||
                         (e as Error).message.includes('Timeout')
                         ||
-                        (e as Error).message.includes('TimeoutError')                      
-                        ) {                         
+                        (e as Error).message.includes('TimeoutError')
+                    ) {
                         await delay(4000, 12000);
                         await page.close();
                         await browser.close();
                         await client.browser.session.stop({ sessionId: session.sessionId });
                         await delay(4000, 12000);
-                        session = await client.browser.session.create();
+                        session = await client.browser.session.create(); //
                         browser = await chromium.connectOverCDP(session.cdpUrl as string);
                         context = browser.contexts()[0];
                         console.log("nova sessão:", session.sessionId);
-                        const page2 = await context.newPage();
-                        const dados = await tentarAcessarServico(page2, service);
+                        page = await context.newPage();
+                        const dados = await tentarAcessarServico(page, service);
                         if (dados) {
                             resultados.push({
                                 nome: service.name,
@@ -127,18 +129,19 @@ export async function checkAllServices() {
 
                             console.log(`💀 ${service.name}: ${dados.status}`);
                         }
-                        await page2.close();
+
                     } else {
                         console.log(e)
                     }
                 }
             } finally {
-                try {
+
+
+                if (page && !page.isClosed()) {
                     await page.close();
-                } catch (error) {
-                    console.log(`não caiu no catch do quic, passa reto`)
                 }
-                await delay(5000, 12000);
+
+
             }
         } //fim loop
         return resultados;
