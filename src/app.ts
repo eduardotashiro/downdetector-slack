2462ab6d7bacdcc06e1f11b3d84590b2d3fa1848
Eduardo Tashiro <eduardogaia854@hotmail.com>
2026-01-22 01:46:05 -0300

    refactor: create service files structure (EMPTY)

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import { App, LogLevel } from "@slack/bolt";
 import { config } from "./config/env.js";
 import cron from "node-cron";
-import { CheckAll } from "./slack/notifier/batchNotifier.js";
+import { CheckAll } from "./slack/batchNotifier.js";
 
 export const app = new App({
   signingSecret: config.slack.signingSecret,
