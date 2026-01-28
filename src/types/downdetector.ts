7fb039846dba61cb5e5f0e8c944eb4df081a2dfb
Eduardo Tashiro <eduardogaia854@hotmail.com>
2026-01-28 01:26:01 -0300

    refactor: clean up ServiceProperties

--- src/types/downdetector.ts
+++ src/types/downdetector.ts
@@ -1,28 +1,9 @@
 export interface ServiceProperties {
-    id: number;
     status: string;
     company: string;
-    max_baseline: number;
-    min_baseline: number;
     max: number;
-    regionalCommunicate: boolean;
-    communicate: {
-        created_at: string;
-        message: string;
-    } | null;
-    relatedCompanies: Array<{
-        name: string;
-        domain: string;
-        url: string;
-        countryIso2: string;
-    }>;
     series: {
         reports: {
-            label: string;
-            data: Array<{ x: string; y: number }>;
-        };
-        baseline: {
-            label: string;
             data: Array<{ x: string; y: number }>;
         };
     };
@@ -31,7 +12,7 @@ export interface ServiceProperties {
 declare global {
     interface Window {
         DD?: {
-            currentServiceProperties?: ServiceProperties;
+            currentServiceProperties: ServiceProperties;
         };
     }
 }
