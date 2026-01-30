33698389ccc08e8a6e2341cb7a869fecdedeee1c
Eduardo Tashiro <eduardogaia854@hotmail.com>
2026-01-30 13:00:49 -0300

    initial class coment..

--- src/slack/incidentMonitor.ts
+++ src/slack/incidentMonitor.ts
@@ -1,11 +1,11 @@
-import { WebClient } from "@slack/web-api";
-import { config } from "../config/env.js";
-import { ServiceStatus } from "./types.js";
+// import { WebClient } from "@slack/web-api";
+// import { config } from "../config/env.js";
+// import { ServiceStatus } from "./types.js";
 
-export class IncidentMonitor {
-  private client: WebClient;
-  private incidente: {
-    inicio: number;
-    nivel: ServiceStatus;
-    alertaEnviado: boolean;
-  } | null = null;
+// export class IncidentMonitor {
+//   private client: WebClient;
+//   private incidente: {
+//     inicio: number;
+//     nivel: ServiceStatus;
+//     alertaEnviado: boolean;
+//   } | null = null;
