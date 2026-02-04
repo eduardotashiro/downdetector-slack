e2134e18eb030f02195689a0c7788c63e67df05d
Eduardo Tashiro <eduardogaia854@hotmail.com>
2026-02-04 01:04:30 -0300

    fix: use thank-position-lens node
    remove country
    
     add type hosted

--- src/pages/batchPages.ts
+++ src/pages/batchPages.ts
@@ -8,14 +8,14 @@ const client = new BrowsercashSDK({
 });
 
 const SERVICES = [
-    { name: "Pix", url: "https://downdetector.com.br/fora-do-ar/pix/"},
-    { name: "Banco Itaú", url: "https://downdetector.com.br/fora-do-ar/banco-itau/"},
-    { name: "Bradesco", url: "https://downdetector.com.br/fora-do-ar/bradesco/"},
-    { name: "Santander", url: "https://downdetector.com.br/fora-do-ar/santander/"},
-    { name: "Nubank", url: "https://downdetector.com.br/fora-do-ar/nubank/"},
-    { name: "Banco do Brasil", url: "https://downdetector.com.br/fora-do-ar/banco-do-brasil/"},
-    { name: "Mercado Pago", url: "https://downdetector.com.br/fora-do-ar/mercadopago/"},
-    { name: "Pic Pay", url: "https://downdetector.com.br/fora-do-ar/picpay/"}
+    { name: "Pix", url: "https://downdetector.com.br/fora-do-ar/pix/" },
+    { name: "Banco Itaú", url: "https://downdetector.com.br/fora-do-ar/banco-itau/" },
+    { name: "Bradesco", url: "https://downdetector.com.br/fora-do-ar/bradesco/" },
+    { name: "Santander", url: "https://downdetector.com.br/fora-do-ar/santander/" },
+    { name: "Nubank", url: "https://downdetector.com.br/fora-do-ar/nubank/" },
+    { name: "Banco do Brasil", url: "https://downdetector.com.br/fora-do-ar/banco-do-brasil/" },
+    { name: "Mercado Pago", url: "https://downdetector.com.br/fora-do-ar/mercadopago/" },
+    { name: "Pic Pay", url: "https://downdetector.com.br/fora-do-ar/picpay/" }
 ];
 
 
@@ -23,26 +23,28 @@ const delay = (min = 4000, max = 9000) => new Promise(res => setTimeout(res, Mat
 
 
 async function checkServiceStatus(page: any, service: { name: string, url: string }) {
+
     await page.goto(service.url, {
         waitUntil: "domcontentloaded",
         timeout: 60000,
     });
 
-    await page.waitForFunction(() => window.DD?.currentServiceProperties !== undefined, { timeout: 30000 });
+    await page.waitForFunction(() => window.DD?.currentServiceProperties !== undefined, { timeout: 60000 });
 
     return await page.evaluate(() => window.DD?.currentServiceProperties);
+
+
 }
 
 
 export async function checkAllServices() {
-    const results:any = [];
+    const results: any = [];
     let session;
     let browser;
     try {
         session = await client.browser.session.create({
-            country:"CA",
-            type: "hosted",
-        });
+    type: "hosted",  // ← Adiciona de volta
+    nodeId: "thank-position-lens",        });
 
         console.log("Session:", session.sessionId);
         console.log("CDP URL:", session.cdpUrl);
@@ -51,18 +53,19 @@ export async function checkAllServices() {
 
         const context = browser.contexts()[0];
 
-   for (const service of SERVICES) {
+        for (const service of SERVICES) {
             const page = await context.newPage();
             try {
                 const data = await checkServiceStatus(page, service);
                 if (data) {
-                    results.push({ 
+                    results.push({
                         name: service.name,
-                        url: service.url, 
-                        data });
-                     console.log(`💀 ${service.name}: ${data.status}`);
+                        url: service.url,
+                        data
+                    });
+                    console.log(`💀 ${service.name}: ${data.status}`);
                 }
-                
+
             } catch (error: any) {
                 console.log(`${service.name}: ${error.message}`);
             } finally {
