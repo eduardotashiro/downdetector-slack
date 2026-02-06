1575e871f89192e1634a665bbba424f36405ce22
Eduardo Tashiro <eduardogaia854@hotmail.com>
2026-02-06 01:15:53 -0300

    feat: add ServiceURL enum

--- src/slack/types.ts
+++ src/slack/types.ts
@@ -14,3 +14,14 @@ export enum ServiceName {
     NUBANK = "Nubank",
     MERCADO_PAGO = "Mercado Pago"
 }
+
+export enum ServiceURL {
+    BANCO_DO_BRASIL = "https://downdetector.com.br/fora-do-ar/banco-do-brasil/",
+    BRADESCO = "https://downdetector.com.br/fora-do-ar/bradesco/",
+    SANTANDER = "https://downdetector.com.br/fora-do-ar/santander/",
+    PIX = "https://downdetector.com.br/fora-do-ar/pix/",
+    PICPAY = "https://downdetector.com.br/fora-do-ar/picpay/",
+    ITAU = "https://downdetector.com.br/fora-do-ar/banco-itau/",
+    NUBANK = "https://downdetector.com.br/fora-do-ar/nubank/",
+    MERCADO_PAGO = "https://downdetector.com.br/fora-do-ar/mercadopago/"
+}
