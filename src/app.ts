b1e1afdc173d6fa1151be428e4bddb20a622dd2a
Eduardo Tashiro <eduardogaia854@hotmail.com>
2026-02-07 00:42:39 -0300

    refactor: move cron from app.ts to jobs/monitoring.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import { App, LogLevel } from "@slack/bolt";
-import { CheckAll } from "./slack/batchNotifier.js";
 import { config } from "./config/env.js";
-import cron from "node-cron";
+
 
 export const app = new App({
   signingSecret: config.slack.signingSecret,
@@ -10,21 +9,6 @@ export const app = new App({
 });
 
 
-cron.schedule("*/15 * * * *", run,
-  {
-    timezone: "America/Sao_Paulo"
-  }
-);
-
-async function run() {
-  try {
-    console.log(`Monitoramento iniciado: ${new Date().toLocaleString("pt-BR", { timeZone: "America/Sao_Paulo" })}`);
-    await CheckAll();
-    console.log(`Monitoramento finalizado: ${new Date().toLocaleString("pt-BR", { timeZone: "America/Sao_Paulo" })}`);
-  } catch (error) {
-    console.error(`Erro no monitoramento:`, error);
-  }
-}
 
 
 
---
+++ src/jobs/monitoring.ts
@@ -0,0 +1,21 @@
+import { CheckAll } from "../slack/batchNotifier.js";
+import cron from "node-cron";
+
+
+
+cron.schedule("*/1 * * * *", run,
+  {
+    timezone: "America/Sao_Paulo"
+  }
+);
+
+
+async function run() {
+  try {
+    console.log(`Monitoramento iniciado: ${new Date().toLocaleString("pt-BR", { timeZone: "America/Sao_Paulo" })}`);
+    await CheckAll();
+    console.log(`Monitoramento finalizado: ${new Date().toLocaleString("pt-BR", { timeZone: "America/Sao_Paulo" })}`);
+  } catch (error) {
+    console.error(`Erro no monitoramento:`, error);
+  }
+};
