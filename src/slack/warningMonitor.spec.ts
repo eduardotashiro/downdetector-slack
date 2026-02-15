3e8e3f59ce1deb0d48859b83bc7e3f98a63f963f
Eduardo Tashiro <eduardogaia854@hotmail.com>
2026-02-15 15:55:54 -0300

    test: setup vitest for unit testing

---
+++ src/slack/warningMonitor.spec.ts
@@ -0,0 +1,14 @@
+import {test,expect } from "vitest"
+
+// @example
+
+// Define a simple test
+test('should add two numbers', () => {
+  expect((1 + 2)).toBe(3);
+});
+
+
+// Define a test with options
+test('should subtract two numbers', { retry: 3 }, () => {
+  expect((5 - 2)).toBe(3);
+});
