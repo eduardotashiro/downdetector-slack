0fb425a3eb053d6b2b2ef91a3f8edd59b3bd5dba
Eduardo Tashiro <eduardogaia854@hotmail.com>
2026-03-28 02:55:20 -0300

    fix: update test

--- src/slack/__tests__/incidentMonitor.spec.ts
+++ src/slack/__tests__/incidentMonitor.spec.ts
@@ -1,85 +1,65 @@
-// import { vi, it, describe, expect } from "vitest";
-// import { IncidentMonitor } from "../incidentMonitor.js";
-// import { ServiceName, ServiceStatus, ServiceURL } from "../types.js";
+import { vi, it, describe, expect } from "vitest";
+import { IncidentMonitor } from "../incidentMonitor.js";
+import { ServiceName, ServiceURL } from "../types.js";
 
-// const payloadDanger = {
-//     name: ServiceName.PIX,
-//     url: ServiceURL.PIX,
-//     data: {
-//         id: 54595,
-//         status: ServiceStatus.DANGER,
-//         company: ServiceName.PIX,
-//         max: 100,
-//         series: {
-//             reports: {
-//                 data: [{ x: "2025-12-11T02:06:52+00:00", y: 100 }],
-//             }
-//         }
-//     }
-// }
+const payloadDanger = {
+    name: ServiceName.BRADESCO,
+    url: ServiceURL.BRADESCO,
+    outage: true
+}
 
-// const payloadSuccess = {
-//     name: ServiceName.PIX,
-//     url: ServiceURL.PIX,
-//     data: {
-//         id: 54595,
-//         status: ServiceStatus.SUCCESS,
-//         company: ServiceName.PIX,
-//         max: 100,
-//         series: {
-//             reports: {
-//                 data: [{ x: "2025-12-11T02:06:52+00:00", y: 100 }],
-//             }
-//         }
-//     }
-// }
+const payloadSuccess = {
+    name: ServiceName.BRADESCO,
+    url: ServiceURL.BRADESCO,
+    outage: false
+}
 
-// describe("IncidentMonitor", () => {
-//     it("should send alert when is DANGER states", async () => {
-//         const mockPostMessage = vi.fn()
-//         const mockClient = {
-//             chat: {
-//                 postMessage: mockPostMessage
-//             }
-//         } as any;
+describe("IncidentMonitor", () => {
+    it("should send alert when is DANGER states", async () => {
+        const mockPostMessage = vi.fn()
+        const mockClient = {
+            chat: {
+                postMessage: mockPostMessage
+            }
+        } as any;
 
-//         const monitor = new IncidentMonitor(mockClient, "C98E7US65TS")
+        const monitor = new IncidentMonitor(mockClient, "C98E7US65TS")
 
-//         await monitor.handle(payloadDanger)
+        await monitor.handle(payloadDanger)
 
-//         expect(mockPostMessage).toHaveBeenCalledOnce();
-//     })
+        expect(mockPostMessage).toHaveBeenCalledOnce();
+    })
 
-//  it("should not send twice when already in incident and is DANGER states", async () => {
-//         const mockPostMessage = vi.fn()
-//         const mockClient = {
-//             chat: {
-//                 postMessage: mockPostMessage
-//             }
-//         } as any;
+    it("should not send twice when already in incident and is DANGER states", async () => {
+        const mockPostMessage = vi.fn()
+        const mockClient = {
+            chat: {
+                postMessage: mockPostMessage
+            }
+        } as any;
 
-//         const monitor = new IncidentMonitor(mockClient, "C98E7US65TS")
+        const monitor = new IncidentMonitor(mockClient, "C98E7US65TS")
 
-//         await monitor.handle(payloadDanger)
-//         await monitor.handle(payloadDanger)
+        await monitor.handle(payloadDanger)
+        await monitor.handle(payloadDanger)
 
-//         expect(mockPostMessage).toHaveBeenCalledOnce();
-//     })
+        expect(mockPostMessage).toHaveBeenCalledOnce();
+    })
 
-//      it("should send resolution when status becomes SUCCESS", async () => {
-//         const mockPostMessage = vi.fn()
-//         const mockClient = {
-//             chat: {
-//                 postMessage: mockPostMessage
-//             }
-//         } as any;
+    it("should send resolution when status becomes SUCCESS", async () => {
+        const mockPostMessage = vi.fn()
+        const mockClient = {
+            chat: {
+                postMessage: mockPostMessage
+            }
+        } as any;
 
-//         const monitor = new IncidentMonitor(mockClient, "C98E7US65TS")
+        const monitor = new IncidentMonitor(mockClient, "C98E7US65TS")
 
-//         await monitor.handle(payloadDanger)
-//         await monitor.handle(payloadSuccess)
+        await monitor.handle(payloadDanger)
+        await monitor.handle(payloadSuccess)
 
-//         expect(mockPostMessage).toHaveBeenCalledTimes(2);
-//     })
+        expect(mockPostMessage).toHaveBeenCalledTimes(2);
+    })
 
-// })
+})
