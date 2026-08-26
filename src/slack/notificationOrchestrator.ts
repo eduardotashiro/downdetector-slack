ef2fc3fb1ac6b43849204c70fae3f76133bcbb25
Eduardo Tashiro <eduardogaia854@hotmail.com>
2026-08-26 17:34:28 -0300

    refactor: eliminates manual work when adding new services and inserts the alerts into the correct file

--- src/slack/notificationOrchestrator.ts
+++ src/slack/notificationOrchestrator.ts
@@ -1,5 +1,7 @@
+import { errorMessageEphemeral } from "../slack/errorMonitor/ephemeralAlert.js"
 import { checkAllServices } from "../services/downdetectorService.js";
 import { IncidentMonitor } from "./incidentMonitor.js";
+import { errorMessageDM } from "../slack/errorMonitor/dmAlert.js"
 import { ServiceName } from "./types.js";
 import { WebClient } from "@slack/web-api";
 import { config } from "../config/env.js";
@@ -7,37 +9,24 @@ import { config } from "../config/env.js";
 const client = new WebClient(config.slack.botToken);
 const channel = config.slack.channel
 
+const monitors = {} as Record<ServiceName, IncidentMonitor>;
 
-const pixMonitor = new IncidentMonitor(client, channel);
-const nubankMonitor = new IncidentMonitor(client, channel);
-const bradescoMonitor = new IncidentMonitor(client, channel);
-const santanderMonitor = new IncidentMonitor(client, channel);
-const bbMonitor = new IncidentMonitor(client, channel);
-const itauMonitor = new IncidentMonitor(client, channel);
-const mercadoPagoMonitor = new IncidentMonitor(client, channel);
-const picpayMonitor = new IncidentMonitor(client, channel);
-
-const monitors: Record<ServiceName, IncidentMonitor> = {
-    [ServiceName.PIX]: pixMonitor,
-    [ServiceName.NUBANK]: nubankMonitor,
-    [ServiceName.BRADESCO]: bradescoMonitor,
-    [ServiceName.SANTANDER]: santanderMonitor,
-    [ServiceName.BANCO_DO_BRASIL]: bbMonitor,
-    [ServiceName.ITAU]: itauMonitor,
-    [ServiceName.MERCADO_PAGO]: mercadoPagoMonitor,
-    [ServiceName.PICPAY]: picpayMonitor
-
-};
+for (const name of Object.values(ServiceName)) {
+    monitors[name] = new IncidentMonitor(client, channel);
+}
 
 export async function CheckAll() {
-
     const servicesResult = await checkAllServices();
+    if (servicesResult.length === 0) {
+        const errorMessage = `Nenhum serviço foi verificado`;
+         await errorMessageEphemeral.handle(errorMessage);
+          await errorMessageDM.handle(errorMessage);
+    }
 
     await Promise.all(servicesResult.map(async (services) => {
         const handler = monitors[services.name];
-        if (handler) {
-            await handler.handle(services);
-        }
+        if (handler) await handler.handle(services);
     }));
 
 }
+
