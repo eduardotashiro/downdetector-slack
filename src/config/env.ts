c7f5c469453b707386108afda7e4f067bc4c686f
Eduardo Tashiro <eduardogaia854@hotmail.com>
2026-08-26 17:29:09 -0300

    chore: env validation

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,11 +1,26 @@
 import dotenv from "dotenv"
 dotenv.config()
 
+const requiredEnv = [
+    "SLACK_SIGNING_SECRET",
+    "SLACK_BOT_TOKEN",
+    "CHANNEL_ID",
+    "USER_ID",
+    "PORT"
+];
+
+const missingEnv = requiredEnv.filter(name => !process.env[name]);
+
+if (missingEnv.length > 0) {
+    throw new Error(`variáveis não encontradas no .env: ${missingEnv.join(", ")} `);
+}
+
 export const config = {
-    slack:{
-        botToken: process.env.SLACK_BOT_TOKEN!,
+    slack: {
         signingSecret: process.env.SLACK_SIGNING_SECRET!,
+        botToken: process.env.SLACK_BOT_TOKEN!,
         channel: process.env.CHANNEL_ID!,
-        userId: process.env.USER_ID!
+        userId: process.env.USER_ID!,
+        port: process.env.PORT!
     }
 }
